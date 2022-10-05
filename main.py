3bef7f3e18c9fda62dd99ae84ac62bc8f0f29e6b
kalculata <nprincehuzaifa@gmail.com>
2022-10-05 17:27:47 +0200

    define functions

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+
+class DNA:
+  def __init__(self):
+    pass
+
+
+class Population:
+  def __init__(self):
+    pass
+
+
+class Utils:
+  @staticmethod
+  def rescale(n, start, stop, new_start, new_stop):
+    pass
+
+
+# parameters
+target = "No matter where you go know that you not alone."
+population_size = 300
+mutation_rate = 0.01
+
+
+# initialization
+population = Population(target, population_size, mutation_rate)
+
+
